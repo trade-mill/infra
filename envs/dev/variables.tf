9411f135a2a73fdd6272596e41fbe29812596d33
k-kbk <kkbk0077@gmail.com>
2025-08-17 22:39:15 +0900

    feat: dev 환경 기본 설정

---
+++ envs/dev/variables.tf
@@ -0,0 +1,14 @@
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+
+variable "aws_profile" {
+  type    = string
+  default = "trademill"
+}
